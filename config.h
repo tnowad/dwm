ffad0e78c43d40d689301b7e0e1244402b3ba0b6
Nguyen Minh Tuan <tnowad@gmail.com>
2024-04-18 21:46:59 +0700

    feat: add rofi command and change terminal key

--- config.h
+++ config.h
@@ -60,11 +60,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
+static const char *roficmd[]  = { "rofi", "-show", "drun", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key                       function        argument */
-	{ MODKEY,                       XK_p,                     spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return,                spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_r,                     spawn,          {.v = roficmd} },
+	{ MODKEY,                       XK_Return,                spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,                     togglebar,      {0} },
 	{ MODKEY,                       XK_j,                     focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,                     focusstack,     {.i = -1 } },
@@ -72,9 +73,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,                     incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,                     setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,                     setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return,                zoom,           {0} },
-	{ MODKEY,                       XK_Tab,                   view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,                     killclient,     {0} },
+	/* { MODKEY,                       XK_Return,                zoom,           {0} }, */
+	/* { MODKEY,                       XK_Tab,                   view,           {0} }, */
 	{ MODKEY,                       XK_t,                     setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,                     setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,                     setlayout,      {.v = &layouts[2]} },
@@ -95,8 +95,11 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                                     6)
 	TAGKEYS(                        XK_8,                                     7)
 	TAGKEYS(                        XK_9,                                     8)
-	{ MODKEY|ShiftMask,             XK_q,                     quit,           {0} },
+	/* { MODKEY|ShiftMask,             XK_e,                     quit,           {0} }, */
+	{ MODKEY|ShiftMask,             XK_c,                     killclient,     {0} },
+  // Layouts
 
+  // Media Controls
   { 0,                            XF86XK_AudioMute,         spawn,          SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle")},
   { 0,                            XF86XK_AudioLowerVolume,  spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%")},
   { 0,                            XF86XK_AudioRaiseVolume,  spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5%")},
