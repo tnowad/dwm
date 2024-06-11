6e640dae7ad42a78b90c5dfc71ed226ba5827648
Nguyen Minh Tuan <tnowad@gmail.com>
2024-06-11 16:29:33 +0700

    feat: change color and add border window

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
@@ -14,11 +14,11 @@ static const char col_gray2[]       = "#aaaaaa";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_gray5[]       = "#ffffff";
-static const char col_cyan[]        = "#ADDBFF";
+static const char col_cyan[]        = "#444444";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray2, col_gray0, col_gray1 },
-	[SchemeSel]  = { col_gray5, col_gray0,  col_cyan  },
+	[SchemeSel]  = { col_gray5, col_gray0, col_cyan  },
 };
 
 /* tagging */
@@ -63,18 +63,18 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "drun", NULL };
-static const char *maimcmd[] = { "maim", "/home/$USER/Pictures/screenshot-$(date +'%Y%m%d-%H%M%S').png", NULL };
-static const char *maim_window_cmd[] = { "maim", "--window", NULL, "/home/$USER/Pictures/screenshot-$(date +'%Y%m%d-%H%M%S').png", NULL };
-static const char *maim_select_cmd[] = { "maim", "--select", "/home/$USER/Pictures/screenshot-$(date +'%Y%m%d-%H%M%S').png", NULL };
-static const char *maim_clipboard_cmd[] = { "maim", "|", "xclip", "-selection", "clipboard", "-t", "image/png", NULL };
-static const char *maim_clipboard_window_cmd[] = { "maim", "--window", NULL, "|", "xclip", "-selection", "clipboard", "-t", "image/png", NULL };
-static const char *maim_clipboard_select_cmd[] = { "maim", "--select", "|", "xclip", "-selection", "clipboard", "-t", "image/png", NULL };
 
+static const char *scrot_cmd[] = { "sh", "-c", "scrot /home/$USER/Pictures/screenshot-$(date +'%Y%m%d-%H%M%S').png", NULL };
+static const char *scrot_window_cmd[] = { "scrot", "--focused", "/home/$USER/Pictures/screenshot-$(date +'%Y%m%d-%H%M%S').png", NULL };
+static const char *scrot_select_cmd[] = { "scrot", "--select", "/home/$USER/Pictures/screenshot-$(date +'%Y%m%d-%H%M%S').png", NULL };
+static const char *scrot_clipboard_cmd[] = { "scrot", "-e", "xsel --clipboard --input --mime-type=image/png < $f", NULL };
+static const char *scrot_clipboard_window_cmd[] = { "scrot", "--focused", "-e", "xsel --clipboard --input --mime-type=image/png < $f", NULL };
+static const char *scrot_clipboard_select_cmd[] = { "scrot", "--select", "-e", "xsel --clipboard --input --mime-type=image/png < $f", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key                       function        argument */
 	{ MODKEY,                       XK_r,                     spawn,          {.v = roficmd} },
-	{ MODKEY,                       XK_Return,                spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return,                spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,                     togglebar,      {0} },
 	{ MODKEY,                       XK_j,                     focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,                     focusstack,     {.i = -1 } },
@@ -82,8 +82,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,                     incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,                     setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,                     setmfact,       {.f = +0.05} },
-	/* { MODKEY,                       XK_Return,                zoom,           {0} }, */
-	/* { MODKEY,                       XK_Tab,                   view,           {0} }, */
+	{ MODKEY,                       XK_Return,                zoom,           {0} },
+	{ MODKEY,                       XK_Tab,                   view,           {0} },
 	{ MODKEY,                       XK_t,                     setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,                     setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,                     setlayout,      {.v = &layouts[2]} },
@@ -116,12 +116,12 @@ static const Key keys[] = {
   { 0,                            XF86XK_MonBrightnessUp,   spawn,          SHCMD("brightnessctl set +2% & statusbar")},
   { 0,                            XF86XK_TouchpadToggle,    spawn,          SHCMD("touchpad-toggle")},
   { 0,                            XF86XK_Launch1,           spawn,          SHCMD("fanctl toggle")},
-  { 0,                            XK_Print,                 spawn,          {.v = maimcmd } },
-  { MODKEY,                       XK_Print,                 spawn,          {.v = maim_window_cmd } },
-  { ShiftMask,                    XK_Print,                 spawn,          {.v = maim_select_cmd } },
-  { ControlMask,                  XK_Print,                 spawn,          {.v = maim_clipboard_cmd } },
-  { ControlMask|MODKEY,           XK_Print,                 spawn,          {.v = maim_clipboard_window_cmd } },
-  { ControlMask|ShiftMask,        XK_Print,                 spawn,          {.v = maim_clipboard_select_cmd } },
+  { 0,                            XK_Print,                 spawn,          {.v = scrot_cmd } },
+  { MODKEY,                       XK_Print,                 spawn,          {.v = scrot_window_cmd } },
+  { ShiftMask,                    XK_Print,                 spawn,          {.v = scrot_select_cmd } },
+  { ControlMask,                  XK_Print,                 spawn,          {.v = scrot_clipboard_cmd } },
+  { ControlMask|MODKEY,           XK_Print,                 spawn,          {.v = scrot_clipboard_window_cmd } },
+  { ControlMask|ShiftMask,        XK_Print,                 spawn,          {.v = scrot_clipboard_select_cmd } },
 };
 
 /* button definitions */
